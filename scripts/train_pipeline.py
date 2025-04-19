7e24755239f74c36c8785d7c80b2e0a231e1b0bd
Emma Rafkin <Emma.Rafkin@jhuapl.edu>
2025-04-19 18:55:38 -0400

    training for babyberta works

--- scripts/train_pipeline.py
+++ scripts/train_pipeline.py
@@ -1,18 +1,18 @@
 
-from transformers import RobertaTokenizerFast, AutoModel, RobertaConfig, DataCollatorForLanguageModeling, Trainer, TrainingArguments
+from transformers import RobertaTokenizerFast, AutoModelForMaskedLM, RobertaConfig, DataCollatorForLanguageModeling, Trainer, TrainingArguments
 import os
-from datasets import Dataset, load_dataset
+from datasets import load_dataset
 import math
 
 def train_model(mlm_prob: float = 0.15):
     tokenizer = RobertaTokenizerFast.from_pretrained("phueb/BabyBERTa-1")
     configuration = RobertaConfig.from_pretrained("phueb/BabyBERTa-1")
-    model = AutoModel.from_config(configuration)
+    model = AutoModelForMaskedLM.from_config(configuration)
 
     tokenizer.pad_token = tokenizer.eos_token
-    data_folder = "./surprisal_curricula_local"
+    data_folder = ... #TODO Swap out with the folder where your curricula data are saved. Mine have "easy", "medium", "hard" folders and each of those have a train.train and dev.dev file
     def preprocess_function(examples):
-        return tokenizer(examples["text"], padding="max_length", truncation=True, max_length=500)
+        return tokenizer(examples["text"], padding="max_length", truncation=True, max_length=128)
     
     data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probability=mlm_prob)
     for curricula in os.listdir(data_folder):
@@ -27,7 +27,7 @@ def train_model(mlm_prob: float = 0.15):
             output_dir="curriculum_learning",
             eval_strategy="epoch",
             learning_rate=2e-5,
-            num_train_epochs=2, # TODO: increase
+            num_train_epochs=1, # TODO: increase?
             weight_decay=0.01,
             push_to_hub=False,
             save_strategy="epoch"
